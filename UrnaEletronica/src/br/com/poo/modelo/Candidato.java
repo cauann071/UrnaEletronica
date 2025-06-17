ee01c2a1ddaa31bec19f60898c290e9a4f1f571c
cauann071 <cauannalmeida12@gmail.com>
2025-06-17 17:09:52 -0300

    Submissao do projeto mais criação da interface

---
+++ UrnaEletronica/src/br/com/poo/modelo/Candidato.java
@@ -0,0 +1,14 @@
+package br.com.poo.modelo;
+
+
+public class Candidato {
+    private String nome;
+    private String sigla;
+    private int numero;
+    
+    public Candidato(){
+        this.nome = nome;
+        this.numero = numero;
+        this.sigla = sigla;
+    }
+}
