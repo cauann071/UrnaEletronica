6d415722064a60b5c0b41fa54f4f201c46c0b83e
SHIBAI-M <kaiquesantanamaia@gmail.com>
2025-06-30 14:34:53 -0300

    Adição de classes e modificações

--- UrnaEletronica/src/br/com/poo/modelo/Candidato.java
+++ UrnaEletronica/src/br/com/poo/modelo/Candidato.java
@@ -1,14 +1,36 @@
 package br.com.poo.modelo;
 
-
 public class Candidato {
+    private Partido partidoCandidato;
     private String nome;
-    private String sigla;
-    private int numero;
+    private int numCandidato;
     
-    public Candidato(){
+    public Candidato(String nome,Partido partidoCandidato, int numCandidato){
         this.nome = nome;
-        this.numero = numero;
-        this.sigla = sigla;
+        this.partidoCandidato = partidoCandidato;
+        this.numCandidato = numCandidato;
     }
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    public String getNome(){
+        return nome; 
+   }
+    
+    public void setNumCandidato(int numCandidato){
+        this.numCandidato = numCandidato;
+    }
+    
+    public int getNumCandidato(){
+        return numCandidato; 
+   }
+
+    @Override
+    public String toString() {
+        
+        return String.format("%d%03d", partidoCandidato.getNumero(), getNumCandidato());
+    }
+    
 }
---
+++ UrnaEletronica/src/br/com/poo/modelo/teste.java
@@ -0,0 +1,9 @@
+package br.com.poo.modelo;
+
+/**
+ *
+ * @author 232.979944
+ */
+public class teste {
+    
+}
